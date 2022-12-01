58a26a7d39760edce1f105316012ebc8e60dd728
David Synnott <dsynnott@twilio.com>
2022-12-01 18:11:21 +0000

    Day 1

--- 2022/1/index.ts
+++ 2022/1/index.ts
@@ -1,4 +1,22 @@
 import { readFileSync } from 'fs';
 
 const file = readFileSync('./input.txt', 'utf8');
-console.log(file);
+
+const stringElves = file.split(/\n\s*\n/);
+const splitStringElf = stringElves.map((str) => str.split('\n'));
+const numberListElves = splitStringElf
+  .map((stringElf) => stringElf.map((stringCal) => parseInt(stringCal, 10)));
+const totalCalsPerElf = numberListElves
+  .map((numberListElf) => numberListElf.reduce((acc, curr) => acc + curr, 0));
+
+// part 1 const maxElfCal = (list: number[]) => list.reduce((acc, curr) => Math.max(acc, curr), 0);
+
+const getTop3 = (list: number[]) => list.reduce((acc, curr) => {
+  const index = acc.findIndex((val) => curr > val);
+  if (index > -1) {
+    acc.splice(index, 0, curr);
+    acc.pop();
+  }
+  return acc;
+}, [0, 0, 0]);
+console.log(getTop3(totalCalsPerElf).reduce((acc, curr) => acc + curr, 0));
